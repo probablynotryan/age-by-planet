bdb93d398fe3ced5cff57359dcec57698ee0bff1
Ryan Bass <theryanbass@gmail.com>
2022-02-04 08:53:01 -0800

    correct wordage on venusAge property test

--- __tests__/age.test.js
+++ __tests__/age.test.js
@@ -8,7 +8,7 @@ it('should create an object with the inputted number as a property of earthAge',
   let age = new Age(10);
   expect(age.earthAge).toEqual(10);
 })
-it('should create an object with the inputted number as a property of earthAge', () => {
+it('should create an object with the 61 as a property of venusAge', () => {
   let age = new Age(100);
   expect(age.venusAge).toEqual(61);
 })

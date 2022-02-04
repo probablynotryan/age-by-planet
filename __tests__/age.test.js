ce63e1fb72fd4e9d0878ad3fdd3a4af112685c66
Ryan Bass <theryanbass@gmail.com>
2022-02-04 08:42:18 -0800

    add beforeEach age objects to tests

--- __tests__/age.test.js
+++ __tests__/age.test.js
@@ -1,6 +1,11 @@
 /* eslint-disable no-undef */
 import Age from './../src/js/age.js';
 
+beforeEach(() => {
+  let age = new Age (10);
+  let olderAge = new Age (80);
+});
+
 describe ('Age', () => {
 
 it('should create an object with the inputted number as a property of earthAge', () => {

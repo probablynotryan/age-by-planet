523d6267d29e42d1081533431abf14191871b6b7
Ryan Bass <theryanbass@gmail.com>
2022-02-04 10:08:43 -0800

    change properties to call yourAge method

--- src/js/age.js
+++ src/js/age.js
@@ -1,9 +1,15 @@
 export default class Age {
   constructor(age) {
-    this.earthAge = age;
-    this.venusAge = Math.floor(age * 0.62);
-    this.mercuryAge = Math.floor(age * 0.24);
-    this.marsAge = Math.floor(age * 1.88);
-    this.jupiterAge = Math.floor(age * 11.86);
+    this.earthAge = this.yourAge(age, 1);
+    this.venusAge = this.yourAge(age, 0.62);
+    this.mercuryAge = this.yourAge(age, 0.24);
+    this.marsAge = this.yourAge(age, 1.88);
+    this.jupiterAge = this.yourAge(age, 11.86);
   }
+
+  // average US life expectancy = 78
+
+    yourAge(age, ageAdjustment){
+      return Math.floor(age * ageAdjustment);
+    }
 }

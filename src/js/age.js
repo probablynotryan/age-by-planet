c4785001970d1146c1c1abbabb7efb716a8430a0
Ryan Bass <theryanbass@gmail.com>
2022-02-04 09:16:11 -0800

    add mercuryAge/marsAge/jupiterAge properties

--- src/js/age.js
+++ src/js/age.js
@@ -1,7 +1,9 @@
 export default class Age {
   constructor(age) {
-    this.age = age;
     this.earthAge = age;
     this.venusAge = Math.floor(age * 0.62);
+    this.mercuryAge = Math.floor(age * 0.24);
+    this.marsAge = Math.floor(age * 1.88);
+    this.jupiterAge = Math.floor(age * 11.86);
   }
 }

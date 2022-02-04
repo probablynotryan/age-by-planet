35ec18a1f2a7732a553c2967c9c0d0760bd5e8a0
Ryan Bass <theryanbass@gmail.com>
2022-02-04 10:21:23 -0800

    adjust tests to account for actual age calculation

--- __tests__/age.test.js
+++ __tests__/age.test.js
@@ -13,23 +13,23 @@ it('should create an object with the inputted number as the property of earthAge
 })
 it('should create an object with the the as a property of venusAge', () => {
   age = new Age(10);
-  expect(age.venusAge).toEqual(6);
-  expect(age.yourAge(10, 0.62)).toEqual(6);
+  expect(age.venusAge).toEqual(16);
+  expect(age.yourAge(10, 0.62)).toEqual(16);
 })
 it('should create an object with the 2 as the property of mercuryAge', () => {
   age = new Age(10);
-  expect(age.mercuryAge).toEqual(2);
-  expect(age.yourAge(10, 0.24)).toEqual(2);
+  expect(age.mercuryAge).toEqual(41);
+  expect(age.yourAge(10, 0.24)).toEqual(41);
 })
 it('should create an object with the 18 as the property of marsAge', () => {
   age = new Age(10);
-  expect(age.marsAge).toEqual(18);
-  expect(age.yourAge(10, 1.88)).toEqual(18);
+  expect(age.marsAge).toEqual(5);
+  expect(age.yourAge(10, 1.88)).toEqual(5);
 })
 it('should create an object with the 118 as the property of jupiterAge', () => {
   age = new Age(10);
-  expect(age.jupiterAge).toEqual(118);
-  expect(age.yourAge(10, 11.86)).toEqual(118);
+  expect(age.jupiterAge).toEqual(0);
+  expect(age.yourAge(10, 11.86)).toEqual(0);
 })
 // (temporarily disabled while classes are corrected to meet objective standard)
 // it('should return a object with 1 as a property of earthAge', () => {

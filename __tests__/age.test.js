e54d76193a83f2da882f12ba6e03b1354a858047
Ryan Bass <theryanbass@gmail.com>
2022-02-04 08:47:10 -0800

    remove beforeEach objects, add venusAge property test

--- __tests__/age.test.js
+++ __tests__/age.test.js
@@ -1,10 +1,6 @@
 /* eslint-disable no-undef */
 import Age from './../src/js/age.js';
 
-beforeEach(() => {
-  let age = new Age (10);
-  let olderAge = new Age (80);
-});
 
 describe ('Age', () => {
 
@@ -12,6 +8,9 @@ it('should create an object with the inputted number as a property of earthAge',
   let age = new Age(10);
   expect(age.earthAge).toEqual(10);
 })
-
+it('should create an object with the inputted number as a property of earthAge', () => {
+  let age = new Age(100);
+  expect(age.venusAge).toEqual(62);
+})
 
 })
